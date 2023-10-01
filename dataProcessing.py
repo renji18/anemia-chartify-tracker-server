334aa15b30dd999929c6f7ec10681b095e3ae5f3
Aadarsh Jha <aadarshjha1401@gmail.com>
2023-10-01 17:52:31 +0530

    corrections in database and csv cleaning

--- dataProcessing.py
+++ dataProcessing.py
@@ -10,14 +10,14 @@ def process_csv_to_json(csv_file):
         cleaned_df = df[14:48].copy()
         cleaned_df.rename(
             columns={
-                "Unnamed: 1": "toddlers",
-                "Unnamed: 2": "children",
-                "Unnamed: 3": "adolescents",
-                "Unnamed: 4": "pregnantWomen",
-                "Unnamed: 5": "mothers",
-                "Unnamed: 6": "indexValue",
-                "Unnamed: 7": "rank",
-                "INDIA": "name",
+                "Unnamed: 1": "Children (6 - 59 months)",
+                "Unnamed: 2": "Children (6 - 9 years)",
+                "Unnamed: 3": "Adolescents (10 - 19 years)",
+                "Unnamed: 4": "Pregnant Women",
+                "Unnamed: 5": "Mothers",
+                "Unnamed: 6": "Index Value",
+                "Unnamed: 7": "Rank",
+                "INDIA": "District",
             },
             inplace=True,
         )
--- dbHandling.py
+++ dbHandling.py
@@ -2,45 +2,77 @@ from pymongo import UpdateOne
 from pymongo.errors import PyMongoError
 
 
+def increment_roman_numeral(roman_numeral):
+    if roman_numeral == "I":
+        return "II"
+    elif roman_numeral == "II":
+        return "III"
+    elif roman_numeral == "III":
+        return "IV"
+    else:
+        raise ValueError("Invalid Roman numeral")
+
+
 def add_to_database(mongo, array_of_dictionaries):
     try:
-        selected_document = array_of_dictionaries[0]["name"]
+        selected_document = array_of_dictionaries[0]["District"]
         collection = mongo.db.anemiaData
-        # bulk_inserts = []
         bulk_updates = []
 
         document = collection.find_one({"state": selected_document})
 
         if document:
-            # update_operations = {"$push": {}}
             data = document.get("data", [])
-            for item in array_of_dictionaries:
-                matching_object = next(
-                    (obj for obj in data if obj.get("name") == item["name"]), None
-                )
-                if matching_object:
-                    for key, value in item.items():
-                        if key != "name":
-                            matching_object.setdefault(key, [])
-                            matching_object[key].append(value)
-                else:
-                    data.append(item)
-            collection.update_one(
-                {"state": selected_document},
-                {"$set": {"data": data}},
+            quarters = document.get("quarters", [])
+        else:
+            data = []
+            quarters = []
+
+        for item in array_of_dictionaries:
+            matching_object = next(
+                (obj for obj in data if obj.get("District") == item["District"]), None
+            )
+            if matching_object:
+                for (
+                    key,
+                    value,
+                ) in item.items():
+                    if key != "District":
+                        matching_object.setdefault(key, [])
+                        matching_object[key].append(value)
+            else:
+                new_item = {"District": item["District"]}
+                for key, value in item.items():
+                    if key != "District":
+                        new_item[key] = [value]
+                data.append(new_item)
+
+        if quarters == []:
+            quarters.append("2021_I")
+        else:
+            last_quarter = quarters[-1]
+            year, roman_numeral = last_quarter.split("_")
+            if roman_numeral == "IV":
+                year = str(int(year) + 1)
+                roman_numeral = "I"
+            else:
+                roman_numeral = increment_roman_numeral(roman_numeral)
+            quarters.append(f"{year}_{roman_numeral}")
+
+        update_document = {
+            "state": selected_document,
+            "data": data,
+            "quarters": quarters,
+        }
+
+        if document:
+            bulk_updates.append(
+                UpdateOne({"state": selected_document}, {"$set": update_document})
             )
         else:
-            new_document = {"state": selected_document, "data": []}
-
-            for item in array_of_dictionaries:
-                new_item = {"name": item["name"]}
-                for key, value in item.items():
-                    if key != "name":
-                        new_item[key] = [value]
-                new_document["data"].append(new_item)
             bulk_updates.append(
                 UpdateOne(
-                    {"state": selected_document}, {"$set": new_document}, upsert=True
+                    {"state": selected_document}, {"$set": update_document}, upsert=True
                 )
             )
 
@@ -55,44 +87,6 @@ def add_to_database(mongo, array_of_dictionaries):
     except Exception as e:
         raise Exception(f"Error processing the file: {str(e)}")
 
-
-# def add_to_database(mongo, array_of_dictionaries):
-#     try:
-#         which_collection = array_of_dictionaries[0]["name"]
-#         collection = mongo.db.anemiaData
-#         bulk_inserts = []
-#         bulk_updates = []
-#         for item in array_of_dictionaries:
-#             document = collection.find_one({"name": item["name"]})
-#             if document:
-#                 update_operations = []
-#                 for key, value in item.items():
-#                     if key != "name":
-#                         update_operations.append(
-#                             UpdateOne({"name": item["name"]}, {"$push": {key: value}})
-#                         )
-#                 bulk_updates.extend(update_operations)
-#             else:
-#                 new_document = {}
-#                 for key, value in item.items():
-#                     if key == "name":
-#                         new_document[key] = value
-#                     else:
-#                         new_document[key] = [value]
-#                 bulk_inserts.append(new_document)
-#         if bulk_inserts:
-#             collection.insert_many(bulk_inserts)
-#         if bulk_updates:
-#             collection.bulk_write(bulk_updates)
-#         if bulk_inserts or bulk_updates:
-#             return {"status": "SUCCESS"}
-#         else:
-#             return {"status": "No data to insert or update"}
-#     except PyMongoError as e:
-#         print(f"MongoDB Error: {str(e)}")
-#         return {"status": "MongoDB Error"}
-#     except Exception as e:
-#         raise Exception(f"Error processing the file: {str(e)}")
 
 
 def read_database(mongo):
